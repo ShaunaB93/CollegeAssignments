65eca948dbcbca186d49b8c91f8111dea5032c15
ShaunaB93 <35816763+ShaunaB93@users.noreply.github.com>
2018-02-08 20:59:01 +0000

    Update fib.py

--- fib.py
+++ fib.py
@@ -17,3 +17,31 @@ def fib(n):
 x = 20
 ans = fib(x)
 print("Fibonacci number", x, "is", ans)
+
+# Shauna Byrne
+# Exercise 2 - Fibonacci numbers using surname.
+
+def fib(n):
+  """This function returns the nth Fibonacci number."""
+  i = 0
+  j = 1
+  n = n - 1
+
+  while n >= 0:
+    i, j = j, i + j
+    n = n - 1
+  
+  return i
+
+name = "Byrne"
+first = name[0]
+last = name[-1]
+firstno = ord(first)
+lastno = ord(last)
+x = firstno + lastno
+
+ans = fib(x)
+print("My surname is", name)
+print("The first letter", first, "is number", firstno)
+print("The last letter", last, "is number", lastno)
+print("Fibonacci number", x, "is", ans)
