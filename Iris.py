de9f65e0155f514259dc1b22d5bdb2ae99618151
ShaunaB93 <35816763+ShaunaB93@users.noreply.github.com>
2018-04-06 20:59:52 +0100

    Update Iris.py

--- Iris.py
+++ Iris.py
@@ -1,9 +1,9 @@
 #Shauna Byrne 03/03/18
 #Iris Data - exercise 5
 
-print("Petal Length", "|", "Petal Width", "|", "Sepal Length","|", "Sepal Width")
-print("-"*60)
-with open("data/iris.csv") as i:
+print("Petal Length", "|", "Petal Width", "|", "Sepal Length","|", "Sepal Width") #Prints heading line added for visual effect on output
+print("-"*60)#Prints dividing line added for visual effect on output
+with open("data/iris.csv") as i: #Opens csv file containing data set
  for line in i:
-  k = (line.split(','))
+  k = (line.split(',')) #splits the information from the file
   print(('{:14s} {:14s} {:14s} {:14s}').format(k[2], k[3], k[0], k[1])) #used 14 for alignment of numbers in output
